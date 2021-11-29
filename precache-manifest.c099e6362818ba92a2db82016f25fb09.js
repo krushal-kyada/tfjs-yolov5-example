0870e58d750da7e0a1b48349b4fb5599091575f2
krushal-kyada <krushal.kyada@intellectdesign.com>
2021-11-29 10:19:20 +0530

    Updates

---
+++ precache-manifest.c099e6362818ba92a2db82016f25fb09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8588fd020c04803c25f8d7e11958316",
+    "url": "/tfjs-yolov5-example/index.html"
+  },
+  {
+    "revision": "d274790b9c284cce9975",
+    "url": "/tfjs-yolov5-example/static/css/main.d13445b9.chunk.css"
+  },
+  {
+    "revision": "44f6591c5de10f69b384",
+    "url": "/tfjs-yolov5-example/static/js/2.e7476062.chunk.js"
+  },
+  {
+    "revision": "85689fe4cf6c0f2cfa4981fee7999b6c",
+    "url": "/tfjs-yolov5-example/static/js/2.e7476062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d274790b9c284cce9975",
+    "url": "/tfjs-yolov5-example/static/js/main.1f6484de.chunk.js"
+  },
+  {
+    "revision": "98e76ef9b36af9192c50",
+    "url": "/tfjs-yolov5-example/static/js/runtime-main.883a1cbe.js"
+  }
+]);
